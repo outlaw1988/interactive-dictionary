72aa8d2e8efb0a34eeecdd23708f2861e4ff27fc
Kuba Zamojski <kuba.zamojski@gmail.com>
2018-04-06 15:39:03 +0200

    success, failure during exam are shown, bug fix in add_new_category

--- intDictApp/views.py
+++ intDictApp/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, redirect
 from .models import Category, Set, Setup, Word, SrcLanguage, TargetLanguage
 from django.http import HttpResponseRedirect
