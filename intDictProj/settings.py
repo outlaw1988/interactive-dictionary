627bf49718789e5344bc83dd3a35d84962e99dfe
Kuba Zamojski <kuba.zamojski@gmail.com>
2018-05-01 15:39:58 +0200

    Database changed to sqlite - pythonanywhere

--- intDictProj/settings.py
+++ intDictProj/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'intDictProj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'intdict',
-        'USER': 'postgres',
-        'PASSWORD': 'fectper8819',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
